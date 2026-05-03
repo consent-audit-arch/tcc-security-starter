541707d7c0f899a753a6e2ee69fa7d8a8e1722ae
r0HERS <re_rohers@hotmail.com>
2026-05-03 14:43:14 -0300

    chore: initial tcc security starter project

---
+++ src/test/java/com/tcc/tcc_security_starter/TccSecurityStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.tcc_security_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccSecurityStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
